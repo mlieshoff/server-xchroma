e5409bca95b3c70f98f5bedc8d89a739f17bc98a
mlieshoff <michael.lieshoff@gmail.com>
2024-11-22 10:00:05 +0100

    Restructure

--- apps/xchroma/dist.sh
+++ apps/xchroma/dist.sh
@@ -16,7 +16,7 @@ mvn clean install
 
 echo encrypt...
 pwd
-java -cp ../server-xchroma/libs/server-1.0.jar server.FileCipher ../server_credentials/conf/xchroma/key backend/target/backend-1.0.0.jar code ../server-xchroma/apps/xchroma/libs/xchroma-encrypted.jar
+java -cp ../server-xchroma/apps/xchroma/libs/server-1.0.jar server.FileCipher ../server_credentials/conf/xchroma/key backend/target/backend-1.0.0.jar code ../server-xchroma/apps/xchroma/libs/xchroma-encrypted.jar
 
 echo copy credentials...
 cp ../server_credentials/conf/xchroma/encrypted/credentials.properties ../server-xchroma/apps/xchroma/conf
