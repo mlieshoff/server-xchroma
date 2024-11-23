a1463fbc36c265a38a6d1784cdcc2d8e7f32d4a8
mlieshoff <michael.lieshoff@gmail.com>
2024-11-23 16:51:53 +0100

    New infra structure

--- apps/xchroma/dist.sh
+++ apps/xchroma/dist.sh
@@ -1,22 +1,42 @@
 #!/bin/bash
 
+APP_NAME=xchroma
+
 source "$HOME/.sdkman/bin/sdkman-init.sh"
+source "../server_credentials/conf/server-vars.sh"
 
 sdk use java 21.0.3-oracle
 
 echo create server libs...
 mvn clean install
-cp target/server-1.0.jar apps/xchroma/libs
 
-echo prepare xchroma release...
-cd ../xchroma
+echo prepare $APP_NAME release...
+cd ../$APP_NAME
 
 echo create jar...
 mvn clean install
 
 echo encrypt...
 pwd
-java -cp ../server-xchroma/apps/xchroma/libs/server-1.0.jar server.FileCipher ../server_credentials/conf/xchroma/key backend/target/backend-1.0.0.jar code ../server-xchroma/apps/xchroma/libs/xchroma-encrypted.jar
+java -cp ../server-$APP_NAME/target/server-1.0.jar server.FileCipher ../server_credentials/conf/$APP_NAME/key backend/target/backend-1.0.0.jar code ../server-$APP_NAME/target/$APP_NAME-encrypted.jar
 
 echo copy credentials...
-cp ../server_credentials/conf/xchroma/encrypted/credentials.properties ../server-xchroma/apps/xchroma/conf
+cp ../server_credentials/conf/$APP_NAME/encrypted/credentials.properties ../server-$APP_NAME/apps/$APP_NAME/conf
+
+echo upload server jar to dropbox...
+curl -X POST https://content.dropboxapi.com/2/files/upload \
+    --header "Authorization: Bearer $DROPBOX_TOKEN" \
+    --header "Dropbox-API-Arg: {\"autorename\":false,\"mode\":\"overwrite\",\"mute\":false,\"path\":\"/Apps/SERVER_TRANSFER/server-1.0.jar\",\"strict_conflict\":false}" \
+    --header "Content-Type: application/octet-stream" \
+    --data-binary @../server-$APP_NAME/target/server-1.0.jar
+
+echo
+echo upload $APP_NAME jar to dropbox...
+curl -X POST https://content.dropboxapi.com/2/files/upload \
+    --header "Authorization: Bearer $DROPBOX_TOKEN" \
+    --header "Dropbox-API-Arg: {\"autorename\":false,\"mode\":\"overwrite\",\"mute\":false,\"path\":\"/Apps/SERVER_TRANSFER/$APP_NAME-encrypted.jar\",\"strict_conflict\":false}" \
+    --header "Content-Type: application/octet-stream" \
+    --data-binary @../server-$APP_NAME/target/$APP_NAME-encrypted.jar
+
+echo
+echo done.
--- apps/xchroma/release.sh
+++ apps/xchroma/release.sh
@@ -4,16 +4,18 @@ source "$HOME/.sdkman/bin/sdkman-init.sh"
 
 sdk use java 21.0.3-oracle
 
-DIRECTORY=/home/xchroma/server
-STATUS=$DIRECTORY/xchroma.pid
-APP=$DIRECTORY/server/apps/xchroma
+APP_NAME=xchroma
+DIRECTORY=/home/$APP_NAME/server
+STATUS=$DIRECTORY/$APP_NAME.pid
+APP_DIR=$DIRECTORY/server/apps/$APP_NAME
 
 cd $DIRECTORY/server
 
 pwd
 
-echo "Refresh from repositiory...."
+echo "Refresh from repository...."
 git pull
+git gc --aggressive --prune
 
 echo "Stop server...."
 if [ -f "$STATUS" ]; then
@@ -28,12 +30,26 @@ if [ -f "$STATUS" ]; then
   done
 fi
 
-cd $APP
+cd $APP_DIR/libs
+
+echo download server jar from dropbox...
+curl -X POST https://content.dropboxapi.com/2/files/download \
+    --header "Authorization: Bearer $DROPBOX_TOKEN" \
+    --header "Dropbox-API-Arg: {\"path\":\"/Apps/SERVER_TRANSFER/server-1.0.jar\"}"
+echo
+
+echo download $APP_NAME jar from dropbox...
+curl -X POST https://content.dropboxapi.com/2/files/download \
+    --header "Authorization: Bearer $DROPBOX_TOKEN" \
+    --header "Dropbox-API-Arg: {\"path\":\"/Apps/SERVER_TRANSFER/$APP_NAME-encrypted.jar\"}"
+echo
+
+cd ..
 
 echo "Decrypt jar..."
-/root/.sdkman/candidates/java/21.0.3-oracle/bin/java -Dlog4j2.formatMsgNoLookups=true -cp libs/server-1.0.jar server.FileCipher conf/key libs/xchroma-encrypted.jar decode libs/xchroma.jar
+/root/.sdkman/candidates/java/21.0.3-oracle/bin/java -Dlog4j2.formatMsgNoLookups=true -cp libs/server-1.0.jar server.FileCipher conf/key libs/$APP_NAME-encrypted.jar decode libs/$APP_NAME.jar
 
 echo "Start server...."
-/root/.sdkman/candidates/java/21.0.3-oracle/bin/java -Dlog4j2.formatMsgNoLookups=true -jar libs/xchroma.jar conf/key conf/config.properties conf/credentials.properties $STATUS &
+/root/.sdkman/candidates/java/21.0.3-oracle/bin/java -Dlog4j2.formatMsgNoLookups=true -jar libs/$APP_NAME.jar conf/key conf/config.properties conf/credentials.properties $STATUS &
 
 exit 1
